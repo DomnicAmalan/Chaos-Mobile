215c3c6b9ca5a2f3771660e93de86f4c30a54664
Keerthieaaswar <keerthieaaswar@gmail.com>
2021-03-25 16:22:20 +0530

    initial structure

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { Text } from "react-native-paper";
+import { useTheme } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
+  const [isFontLoaded, setFontLoaded] = useState(false);
+  const { fonts } = useTheme();
+
+  const _loadResourcesAsync = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        ColabBol: require("./assets/fonts/ColabBol.otf"),
+        SFSportsNightNSUpright: require("./assets/fonts/SFSportsNightNSUpright.ttf"),
+      }),
+    ]);
+  };
+
+  useEffect(() => {
+    _loadResourcesAsync().then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
+
+  if (!isFontLoaded) {
+    return <View></View>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={{fontFamily:fonts.b}}>Test</Text>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
